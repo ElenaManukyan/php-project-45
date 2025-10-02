cd5efbfd6d0a702f51a73a8c8f6cea89b150196d
Elena <utv.header@gmail.com>
2025-10-02 22:00:53 +0300

    add codesniffer and linter

--- src/Cli.php
+++ src/Cli.php
@@ -1,10 +1,12 @@
 <?php
-namespace BrainGames\Cli; 
+
+namespace BrainGames\Cli;
 
 use function cli\line;
 use function cli\prompt;
 
-function greeting() {
+function greeting()
+{
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
