58110705d572dd75d58f65f34f23fc875ef84f15
Elena <utv.header@gmail.com>
2025-10-06 22:07:12 +0300

    little changes

--- src/Cli.php
+++ src/Cli.php
@@ -5,10 +5,11 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-$name;
+$name = '';
 
 function greeting()
 {
+    global $name;
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
@@ -16,9 +17,11 @@ function greeting()
 
 function tryAgain()
 {
+    global $name;
     line("Let's try again, %s!", $name);
 }
 
 function congrats() {
+    global $name;
     line("Congratulations, %s!", $name);
 }
