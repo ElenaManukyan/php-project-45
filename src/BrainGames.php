40057ff75bd677ae631450c01d173df813357da8
Elena <utv.header@gmail.com>
2025-10-12 15:32:55 +0300

    fix: add new logic

---
+++ src/BrainGames.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BrainGames\BrainGames;
+
+use function cli\line;
+use function cli\prompt;
+use function BrainGames\Engine\greeting;
+use function BrainGames\Engine\tryAgain;
+use function BrainGames\Engine\congrats;
+use function BrainGames\Cli\welcome;
+
+function brainGames()
+{
+    welcome();
+    $name = prompt('May I have your name?');
+    greeting($name);
+    line('Answer "yes" if the number is even, otherwise answer "no".');
+}
